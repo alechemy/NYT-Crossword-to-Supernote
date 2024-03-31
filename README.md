2de4774bfff20f77e9204328b2ba105ef73c2dda
Alec Custer <alcuster@icloud.com>
2024-03-31 07:17:00 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NYT Crossword to Supernote
+
+Tweaked version of [this script](https://gist.github.com/nathanbuchar/8c7a87a8383ee83c7c636f861b0d86a0)
+which uses cron on a simple Node server to fetch the daily NYT crossword and upload it to Dropbox,
+for consumption on a Supernote device.
---
+++ index.js
@@ -0,0 +1,160 @@
+/**
+ * Schedules a cronjob which will automatically download
+ * the NYT daily crossword 1 minute after its published and
+ * then upload it to your Dropbox.
+ *
+ * @author Nathan Buchar <hello@nathanbuchar.com>
+ * @author Alec Custer <alcuster@icloud.com>
+ * @license MIT
+ */
+
+const cron = require("cron");
+const dropbox = require("dropbox");
+const https = require("https");
+const moment = require("moment");
+const path = require("path");
+
+// Instantiate the Dropbox instance.
+//
+// Set DROPBOX_ACCESS_TOKEN to an access token that you've
+// generated for your Dropbox account.
+//
+// See https://dropbox.tech/developers/generate-an-access-token-for-your-own-account
+const dbx = new dropbox.Dropbox({
+  accessToken: process.env.DROPBOX_ACCESS_TOKEN,
+});
+
+async function getPuzzleId(date) {
+  return new Promise((resolve) => {
+    https.get(
+      `https://www.nytimes.com/svc/crosswords/v6/puzzle/daily/${date.format(
+        "YYYY-MM-DD"
+      )}.json`,
+      {
+        headers: {
+          Cookie: process.env.NYT_COOKIE,
+        },
+      },
+      (res) => {
+        let data = "";
+
+        res.on("data", (chunk) => {
+          data += chunk;
+        });
+
+        res.on("end", () => {
+          console.log(`Crossword ID: ${JSON.parse(data).id}`);
+          const id = JSON.parse(data).id;
+          resolve(id);
+        });
+      }
+    );
+  });
+}
+
+function getNYTCrossword(date) {
+  console.log("Attempting to download crossword...");
+  const d = moment(date);
+
+  getPuzzleId(d).then((id) => {
+    const req = https.request(
+      {
+        protocol: "https:",
+        host: "www.nytimes.com",
+        path: `/svc/crosswords/v2/puzzle/${id}.pdf`,
+        method: "GET",
+        headers: {
+          Referer: "https://www.nytimes.com/crosswords/archive/daily",
+          Cookie: process.env.NYT_COOKIE,
+        },
+      },
+      (res) => {
+        if (res.statusCode === 200) {
+          const data = [];
+
+          res.on("error", (err) => {
+            console.log(err);
+          });
+
+          res.on("data", (chunk) => {
+            data.push(chunk);
+          });
+
+          res.on("end", () => {
+            console.log("Successfully downloaded crossword");
+
+            // The file has successfully downloaded. Upload it
+            // to Dropbox.
+            //
+            // Set SUPERNOTE_UPLOAD_PATH to the path where the
+            // PDFs should be uploaded on Dropbox. This should
+            // look something like "/Supernote/Document/Crosswords".
+            dbx
+              .filesUpload({
+                path: path.join(
+                  process.env.SUPERNOTE_UPLOAD_PATH,
+                  `${d.format("YYYYMMDD_ddd")}-crossword.pdf`
+                ),
+                contents: Buffer.concat(data),
+              })
+              .then((response) => {
+                console.log("Successfully uploaded crossword");
+                console.log(`Content hash: ${response.result.content_hash}`);
+              })
+              .catch((err) => {
+                console.log("Error writing to dropbox");
+                console.log(err);
+              });
+          });
+        } else {
+          console.log(
+            `Could not get crossword. Status code: ${res.statusCode}`
+          );
+        }
+      }
+    );
+
+    req.on("error", (err) => {
+      console.log(err);
+    });
+
+    req.end();
+  });
+
+  // Get the crossword.
+  //
+  // Set NYT_COOKIE to the return value of `document.cookie`
+  // when logged into your account on nytimes.com. This
+  // cookie will eventually expire and need to be set again.
+}
+
+function getTomorrowsNYTCrossword() {
+  const today = new Date();
+  const todayNYTime = today.toLocaleString("en-US", {
+    timeZone: "America/New_York",
+  });
+
+  const tomorrow = new Date(todayNYTime);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+
+  getNYTCrossword(tomorrow);
+}
+
+const weekdayJob = cron.CronJob.from({
+  cronTime: "1 22 * * 1,2,3,4,5", // Mon-Fri @ 10:01pm ET.
+  timeZone: "America/New_York",
+  onTick() {
+    getTomorrowsNYTCrossword();
+  },
+});
+
+const weekendJob = cron.CronJob.from({
+  cronTime: "1 18 * * 0,6", // Sun,Sat @ 6:01pm ET.
+  timeZone: "America/New_York",
+  onTick() {
+    getTomorrowsNYTCrossword();
+  },
+});
+
+weekdayJob.start();
+weekendJob.start();
